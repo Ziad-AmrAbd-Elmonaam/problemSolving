c65897f82b228f9cfe0354b7c1fdacb61df642fb
Ziad Ame <ziad_amr@unifisolutions.io>
2023-10-03 16:29:54 +0300

    NumberOfGoodPairs update

--- NumberOfGoodPairs.java
+++ NumberOfGoodPairs.java
@@ -1,18 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
 
-
-
+//   int[] nums = {1,2,3,1,1,3};
 class NumberOfGoodPairs {
-    public int numIdenticalPairs(int[] nums) {
+    public int numIdenticalPairs(int[] A) {
         int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    count++;
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < A.length; i++) {
+            if (map.containsKey(A[i])) {
+                count += map.get(A[i]);
+                map.put(A[i], map.get(A[i]) + 1);
+            } else {
+                map.put(A[i], 1);
             }
+
         }
         return count;
-
     }
-}
 
+}
