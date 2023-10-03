c0a110d83b43ac392d47b68b44fd2ab50893926e
Ziad Ame <ziad_amr@unifisolutions.io>
2023-10-03 11:53:36 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Example usage of the numIdenticalPairs method
+        int[] nums = {1,1,1,1};
+        Solution solution = new Solution();
+        int result = solution.numIdenticalPairs(nums);
+        System.out.println("Number of Identical Pairs: " + result);
+    }
+}
