1d1946f5ac8db5af8a8445535c5b9a92c0d44093
Ziad Ame <ziad_amr@unifisolutions.io>
2023-10-03 16:30:05 +0300

    NumberOfGoodPairs update

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Example usage of the numIdenticalPairs method
-        int[] nums = {1,1,1,1};
+        int[] nums = { 1, 2, 3, 1, 1, 3 };
         NumberOfGoodPairs solution = new NumberOfGoodPairs();
         int result = solution.numIdenticalPairs(nums);
         System.out.println("Number of Identical Pairs: " + result);
